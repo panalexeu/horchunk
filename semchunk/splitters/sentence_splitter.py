4d8b53308076fedf8d4f2cc26e4b5312265d2e4b
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-01-07 17:38:04 +0200

    added docstring for SentenceSplitter

--- semchunk/splitters/sentence_splitter.py
+++ semchunk/splitters/sentence_splitter.py
@@ -4,6 +4,8 @@ from .base import BaseSplitter
 
 
 class SentenceSplitter(BaseSplitter):
+    """Split text into sentences using punctuation marks (.!?)."""
+
     def __call__(self) -> list[str]:
         pattern = r'(?<=[.!?])\s+'
         return re.split(pattern, self.text)
