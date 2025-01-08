cd7c95e8c25cbe9002b32a2edf6c53d6c947332c
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-01-08 12:25:25 +0200

    created binary tuning for the WindowChunker

--- semchunk/chunkers/window.py
+++ semchunk/chunkers/window.py
@@ -1,3 +1,5 @@
+import math
+
 from chromadb import EmbeddingFunction
 
 from .chunk import Chunk
@@ -49,3 +51,67 @@ class WindowChunker(BaseChunker):
 
         # map chunks to list[Chunk]
         return [Chunk(splits=[chunk]) for chunk in chunks]
+
+    def tune(self, splits: list[str], depth: int = 6) -> float:
+        """
+        Tunes window chunker threshold, by calculating the distance for a bunch of chunks with the ``depth`` splits size.
+
+        Chunks are then sorted with the relevant distances. With the binary search algorithm you decide wich thresh
+        should be included, by analyzing created chunks and distance within it.
+        """
+
+        if len(splits) < depth:
+            raise ValueError(
+                "The length of 'splits' is smaller than the defined depth. "
+                "Increase the size of the 'splits' list or decrease the 'depth' parameter."
+            )
+
+        # consecutively forming key: [distance], value: [chunk] dictionary
+        chunks = dict()
+        for i in range(0, len(splits), depth):
+            init = splits[i]
+            res = ' '.join(splits[i:i + depth])
+
+            dist = cosine_dist(
+                self.ef([init])[0],
+                self.ef([res])[0]
+            )
+
+            chunks[dist] = res
+
+        # binary search, with the user evaluation
+        sorted_keys = sorted(chunks.keys())
+
+        print(f'{len(sorted_keys)} chunks formed')
+        print(f'Values range: [{sorted_keys[0]} ... {sorted_keys[-1]}]')
+        time_complex = int(math.log(len(sorted_keys), 2))
+        print(f'Steps to tune: {time_complex}')
+        print('-' * 16)
+
+        low = 0
+        high = len(sorted_keys) - 1
+        dist = None
+        while low < high:
+            mid = (low + high) // 2
+            dist = sorted_keys[mid]
+            chunk = chunks[dist]
+
+            # reading user evaluation
+            print(f'dist: {dist}')
+            print(f'chunk: {chunk}')
+            input_ = str(input("Type 'h' to raise thresh, or 'l' - to lower it: "))
+            print('=' * 32)
+
+            # based on input_ decide what to do next
+            match input_:
+                case 'h':
+                    low = mid + 1
+                case 'l':
+                    high = mid - 1
+                case _:
+                    raise IOError('Irrelevant symbol')
+
+        # tuning complete
+        print(f'Tuning ended, thresh value: {dist}')
+
+        return dist
---
+++ tests/chunkers/test_win_chunker.py
@@ -0,0 +1,25 @@
+import pytest
+from chromadb.utils import embedding_functions
+from rich import print
+
+from semchunk.chunkers import WindowChunker
+
+
+@pytest.fixture
+def win_chunker():
+    ef = embedding_functions.DefaultEmbeddingFunction() # all-MiniLM-L6-v2
+    return WindowChunker(ef)
+
+
+def test_successfully_chunks(win_chunker):
+    splits = ['Hey!', 'Andromeda']
+
+    chunks = win_chunker(splits)
+    chunks = [chunk.splits[0] for chunk in chunks]
+
+    assert sorted(chunks) == sorted(splits)
+
+
+def test_tune_is_not_performed_on_splits_list_smaller_then_depth(win_chunker):
+    with pytest.raises(ValueError):
+        win_chunker.tune(splits=['hey', 'hey'], depth=3)
