67e1701980b48ae4a26ec8881bad5bb0e5f7b616
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-01-11 14:12:21 +0200

    project refactoring

--- semchunk/chunkers/__init__.py
+++ semchunk/chunkers/__init__.py
@@ -1,9 +1,12 @@
 from .base import BaseChunker
 from .chunk import Chunk
 from .win import WindowChunker
+from .dist import CosineDistance, DistanceStrategy
 
 __all__ = [
     'BaseChunker',
     'Chunk',
-    'WindowChunker'
+    'WindowChunker',
+    'CosineDistance',
+    'DistanceStrategy'
 ]
